bfc496d1fd8086dfc29959172382792e7fa00eb0
pwaguzi <saisishaun4@gmail.com>
2024-06-30 11:21:35 +0300

    added the main tables of the product and product category

--- multivendor_ecommerce/backend_api/main/models.py
+++ multivendor_ecommerce/backend_api/main/models.py
@@ -6,3 +6,20 @@ from django.contrib.auth.models import User
 class Vendor(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     address=models.TextField(null=True)
+
+#Category
+class ProductCategory(models.Model):
+    title=models.CharField(max_length=200)
+    detail=models.TextField(null=True)
+    
+    def __str__(self):
+        return self.title
+    
+#Product
+class Product(models.Model):
+    title=models.CharField(max_length=200)
+    detail=models.TextField(null=True)
+    price=models.FloatField
+    
+    def __str__(self):
+        return self.title
