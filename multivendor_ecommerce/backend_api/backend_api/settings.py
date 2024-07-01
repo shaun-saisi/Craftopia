debadbca9ea3c3ac7fd73c071afd63be2f15bee0
pwaguzi <saisishaun4@gmail.com>
2024-07-01 13:21:26 +0300

    added pagination

--- multivendor_ecommerce/backend_api/backend_api/settings.py
+++ multivendor_ecommerce/backend_api/backend_api/settings.py
@@ -136,4 +136,6 @@ REST_FRAMEWORK = {
     #'DEFAULT_AUTHENTICATION_CLASSES': [
      #   'rest_framework_simplejwt.authentication.JWTAuthentication',  
     #],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1
 }
