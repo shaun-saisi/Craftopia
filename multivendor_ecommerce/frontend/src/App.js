aafa5686a95076cb9e168270e52c2bdc9ff24a22
pwaguzi <saisishaun4@gmail.com>
2024-07-02 11:44:38 +0300

    addded the navbar for craftopia

--- multivendor_ecommerce/frontend/src/App.js
+++ multivendor_ecommerce/frontend/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
+        <div className="container">
+          <a className="navbar-brand" href="#">Craftopia</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Categories</a>
+              </li>
+    
+              
+            </ul>
+           
+          </div>
+        </div>
+      </nav>
+    
   );
 }
 
