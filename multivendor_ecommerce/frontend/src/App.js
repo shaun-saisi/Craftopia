1ab26ccfcc96b5eae475ced114295643b237afa6
pwaguzi <saisishaun4@gmail.com>
2024-07-02 13:34:34 +0300

    added the home page and basic card layouts

--- multivendor_ecommerce/frontend/src/App.js
+++ multivendor_ecommerce/frontend/src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-  
+    <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
         <div className="container">
           <a className="navbar-brand" href="#">Craftopia</a>
@@ -19,14 +19,132 @@ function App() {
               <li className="nav-item">
                 <a className="nav-link" href="#">Categories</a>
               </li>
-    
-              
             </ul>
-           
           </div>
         </div>
       </nav>
-    
+
+      <main className="mt-4">
+        <div className="container">
+          <h3 className="mb-4">Latest Products <a href="#" className='float-end btn btn-sm btn-warning mt-2'>View all products</a></h3>
+          <div className="row">
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title text-warning">Product title</h4>
+                  <h5 className="card-title">Price: <span className="text-muted">Ksh. 500</span></h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                  <button title="Add to WishList" className="btn btn-sm ms-2"><i className="fa-solid fa-heart"></i></button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title text-warning">Product title</h4>
+                  <h5 className="card-title">Price: <span className="text-muted">Ksh. 500</span></h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                  <button title="Add to WishList" className="btn btn-sm ms-2"><i className="fa-solid fa-heart"></i></button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title text-warning">Product title</h4>
+                  <h5 className="card-title">Price: <span className="text-muted">Ksh. 500</span></h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                  <button title="Add to WishList" className="btn btn-sm ms-2"><i className="fa-solid fa-heart"></i></button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title text-warning">Product title</h4>
+                  <h5 className="card-title">Price: <span className="text-muted">Ksh. 500</span></h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                  <button title="Add to WishList" className="btn btn-sm ms-2"><i className="fa-solid fa-heart"></i></button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title text-warning">Product title</h4>
+                  <h5 className="card-title">Price: <span className="text-muted">Ksh. 500</span></h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                  <button title="Add to WishList" className="btn btn-sm ms-2"><i className="fa-solid fa-heart"></i></button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title text-warning">Product title</h4>
+                  <h5 className="card-title">Price: <span className="text-muted">Ksh. 500</span></h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                  <button title="Add to WishList" className="btn btn-sm ms-2"><i className="fa-solid fa-heart"></i></button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title text-warning">Product title</h4>
+                  <h5 className="card-title">Price: <span className="text-muted">Ksh. 500</span></h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                  <button title="Add to WishList" className="btn btn-sm ms-2"><i className="fa-solid fa-heart"></i></button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3 mb-4">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title text-warning">Product title</h4>
+                  <h5 className="card-title">Price: <span className="text-muted">Ksh. 500</span></h5>
+                </div>
+                <div className="card-footer">
+                  <button title="Add to Cart" className="btn btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                  <button title="Add to WishList" className="btn btn-sm ms-2"><i className="fa-solid fa-heart"></i></button>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
