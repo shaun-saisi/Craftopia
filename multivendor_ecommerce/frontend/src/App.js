79373543f25e81598af3e628523fb5b2d07083de
pwaguzi <saisishaun4@gmail.com>
2024-07-03 08:50:47 +0300

    added the footer to the homepage

--- multivendor_ecommerce/frontend/src/App.js
+++ multivendor_ecommerce/frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Header from './components/Header';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   return (
@@ -322,6 +324,81 @@ function App() {
 
             </div>
            {/* Popular Sellers end */}
+
+           {/* Rating and Reviews */}
+           <div id="carouselExampleIndicators" className="carousel slide my-4 border bg-dark text-white p-5" data-bs-ride="carousel">
+              <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <figure className="text-center">
+                    <blockquote className="blockquote">
+                      <p> A well known qote that I have written</p>
+                    </blockquote>
+                    <figcaption className="blockquote-footer">
+                      <i className='fa fa-star text warning'></i>
+                      <cite title="Source Title">Customer Name</cite>
+                    </figcaption>
+                  </figure>
+                </div>
+                <div className="carousel-item">
+                <figure className="text-center">
+                    <blockquote className="blockquote">
+                      <p> A well known qote that I have written</p>
+                    </blockquote>
+                    <figcaption className="blockquote-footer">
+                      <i className='fa fa-star text-warning'></i>
+                      <cite title="Source Title">Customer Name</cite>
+                    </figcaption>
+                  </figure>
+                </div>
+                <div className="carousel-item">
+                <figure className="text-center">
+                    <blockquote className="blockquote">
+                      <p> A well known qote that I have written</p>
+                    </blockquote>
+                    <figcaption className="blockquote-footer">
+                      <i className='fa fa-star text-warning'></i>
+                      <cite title="Source Title">Customer Name</cite>
+                    </figcaption>
+                  </figure>
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+
+           {/*End rating and reviews */}
+
+           <footer className='d-flex flex-wrap justify-content-between align-items-center  my-5 border-top'>
+            <div className="col-md -4 d-flex align-items-center">
+              <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-l">
+                Craftopia
+              </a>
+              <span className="mb-3 mb-md-0 text-muted"> 2024 Craft Inc</span>
+            </div>
+
+            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex pt-2">
+              <li className="ms-3">
+                <a className="text-muted" href="#"> <i className='fa-brands fa-facebook fa-2x'></i></a>
+              </li>
+              <li className="ms-3">
+                <a className="text-muted" href="#"> <i className='fa-brands fa-instagram fa-2x'></i></a>
+              </li>
+              <li className="ms-3">
+                <a className="text-muted" href="#"> <i className='fa-brands fa-twitter fa-2x'></i></a>
+              </li>
+            </ul>
+           </footer>
 
         </div>
       </main>
